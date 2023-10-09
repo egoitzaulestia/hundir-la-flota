668863abf19f4d1dd9c0b48a10d26d7cb3e050ce
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-10-09 04:33:07 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Dimensiones del tablero
+DIMENSIONES_TABLERO = 10
+
+# Tipos de barcos y sus tamaños
+BARCOS = {
+    "Barco1": 1,  # Barco de longitud 1
+    "Barco2": 2,  # Barco de longitud 2
+    "Barco3": 3,  # Barco de longitud 3
+    "Barco4": 4   # Barco de longitud 4
+}
