3137d86bb023ce1cd76c99971e6440067048ad18
EGOITZ AULESTIA PADILLA <egoitz.aulestia@ikasle.aeg.eus>
2023-10-09 08:47:18 +0200

    lasta changes

--- variables.py
+++ variables.py
@@ -4,7 +4,13 @@ DIMENSIONES_TABLERO = 10
 # Tipos de barcos y sus tamaños
 BARCOS = {
     "Barco1": 1,  # Barco de longitud 1
-    "Barco2": 2,  # Barco de longitud 2
-    "Barco3": 3,  # Barco de longitud 3
-    "Barco4": 4   # Barco de longitud 4
+    "Barco2": 1,  # Barco de longitud 1
+    "Barco3": 1,  # Barco de longitud 1
+    "Barco4": 1,  # Barco de longitud 1
+    "Barco5": 2,  # Barco de longitud 2
+    "Barco6": 2,  # Barco de longitud 2
+    "Barco7": 2,  # Barco de longitud 2
+    "Barco8": 3,  # Barco de longitud 3
+    "Barco9": 3,  # Barco de longitud 3
+    "Barco10": 4   # Barco de longitud 4
 }
